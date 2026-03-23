c9867d8bd9916da63cd477629127caa458fddaef
Danny <dangfender935@gmail.com>
2026-03-23 23:48:21 +0000

    SUCCESS: Project 6 - Managed to go 730 mm forward and 730 mm back with a fairly large error, but the bot actually stops when it overshoots

--- pose_controller/src/pose_controller.cpp
+++ pose_controller/src/pose_controller.cpp
@@ -4,13 +4,22 @@
 #include <geometry_msgs/Point.h>
 #include <math.h>
 
-#define K1      (1)
+#define K1      (0.9)
 #define K2      (1)
 #define K3      (1)
 #define MAX_X_VEL   (100)
-#define MAX_TH_VEL  (0.5)
-#define DISTANCE_ERROR_THRESHOLD    (1.0)
-#define ANGLE_ERROR_THRESHOLD       (0.07)
+#define MAX_TH_VEL  (0.75)
+#define DISTANCE_ERROR_THRESHOLD    (9.0)
+#define ANGLE_ERROR_THRESHOLD       (0.03)
+#define OVERSHOOT_THRESHOLD         (0.5)
+
+typedef enum MoveState
+{
+    INACTIVE,
+    CORRECT_DIRECTION_ERROR,
+    CORRECT_DISTANCE_ERROR,
+    CORRECT_ORIENTATION_ERROR
+} MoveState;
 
 ros::Subscriber desired_pose_sub;
 ros::Subscriber curr_pose_sub;
@@ -22,57 +31,123 @@ geometry_msgs::Point curr_pose{};
 float desired_global_x = 0.f;
 float desired_global_y = 0.f;
 float desired_global_th = 0.f;
-bool active_state = false;
+MoveState move_state = INACTIVE;
 
 void desired_pose_recv_callback(const geometry_msgs::Vector3& msg)
 {
-    ROS_INFO("pose received!!!");
-    active_state = true;
+    ROS_INFO("============= Pose received! Attempting to reach goal... =============");
     desired_pose = msg;
     desired_global_x = desired_pose.x * cos(curr_pose.z) - desired_pose.y * sin(curr_pose.z) + curr_pose.x;
     desired_global_y = desired_pose.x * sin(curr_pose.z) + desired_pose.y * cos(curr_pose.z) + curr_pose.y;
     desired_global_th = desired_pose.z + curr_pose.z;
     ROS_INFO("Desired global pose: %f %f %f", desired_global_x, desired_global_y, desired_global_th);
-    active_state = true;
+    ROS_INFO("======================================================================");
+    move_state = CORRECT_DIRECTION_ERROR;
     return;
+}
+
+inline float normalize_angle(float angle)
+{
+    while (angle > M_PI) angle -= 2*M_PI;
+    while (angle < -M_PI) angle += 2*M_PI;
+    return angle;
 }
 
 void curr_pose_recv_callback(const pose_est::pose_est_msg& msg)
 {
-    if (!active_state) return;
+    float dx = 0.f;
+    float dy = 0;
+    float rho = 0.f;
+    float alpha = 0.f;
+    float eta= 0.f;
+    float speed = 0.f;
+    static float last_rho;
     curr_pose = msg.point;
-    float dx = desired_global_x - curr_pose.x;
-    float dy = desired_global_y - curr_pose.y;
-    float rho = sqrt(pow(dx, 2) + pow(dy, 2));
-    float alpha = atan2(dy, dx) - curr_pose.z;
-    float eta = desired_global_th - curr_pose.z;
-    ROS_INFO("desired global x - curr pose x: %f - %f = %f", desired_global_x, curr_pose.x, dx);
-    ROS_INFO("rho = %f", rho);
-
-
-    if (rho < DISTANCE_ERROR_THRESHOLD) 
+    switch (move_state)
     {
-        ROS_INFO("Arrived!");
-        rho = 0.f;
-        alpha = 0.f;
+    case CORRECT_DIRECTION_ERROR:
+        dx = desired_global_x - curr_pose.x;
+        dy = desired_global_y - curr_pose.y;
+        alpha = normalize_angle(atan2(dy, dx) - curr_pose.z);
+        ROS_INFO("ALPHA = %f", alpha);
+
+        if (abs(alpha) < ANGLE_ERROR_THRESHOLD) 
+        {
+            move_state = CORRECT_DISTANCE_ERROR;
+            msg_out.z = 0.f;
+            rho = sqrt(pow(dx, 2) + pow(dy, 2));
+            if (rho < DISTANCE_ERROR_THRESHOLD)
+            {
+                move_state = CORRECT_ORIENTATION_ERROR;
+                msg_out.x = 0.f;
+            }
+            else
+            {
+                speed = K1*rho;
+                msg_out.x = (speed > MAX_X_VEL) ? MAX_X_VEL : speed;
+                last_rho = rho;
+                break;
+            }
+            msg_out.z = 0.f;
+            last_rho = 0.f;
+        }
+        else 
+        {
+            speed = K2*alpha;
+            ROS_INFO("Speed = %f", speed);
+            msg_out.z = (speed > MAX_TH_VEL) ? MAX_TH_VEL : speed;
+        }
+        ROS_INFO("msg_out = %f %f %f", msg_out.x, msg_out.y, msg_out.z);
+        break;
+
+    case CORRECT_DISTANCE_ERROR:
+        dx = desired_global_x - curr_pose.x;
+        dy = desired_global_y - curr_pose.y;
+        rho = sqrt(pow(dx, 2) + pow(dy, 2));
+        speed = K1*rho;
+        ROS_INFO("RHO = %f   Last RHO = %f", rho, last_rho);
+        msg_out.x = (speed > MAX_X_VEL) ? MAX_X_VEL : speed * (rho > DISTANCE_ERROR_THRESHOLD) * (last_rho > rho);
+        ROS_INFO("msg_out = %f %f %f", msg_out.x, msg_out.y, msg_out.z);
+
+        if (msg_out.x == 0.0)
+        {
+            move_state = CORRECT_ORIENTATION_ERROR;
+            last_rho = 0.f;
+        }
+        else
+        {
+            last_rho = rho;
+            break;
+        }
+
+    case CORRECT_ORIENTATION_ERROR:
+        eta = desired_global_th - curr_pose.z;
+        ROS_INFO("msg_out = %f %f %f", msg_out.x, msg_out.y, msg_out.z);
+        if (abs(eta) < ANGLE_ERROR_THRESHOLD)
+        {
+            move_state = INACTIVE;
+            msg_out.z = 0.f;
+            ROS_INFO("======================================================================");
+            ROS_INFO("============================= Arrived!!! =============================");
+            ROS_INFO("======================================================================");
+        }
+        else
+        {
+            speed = K3*eta;
+            msg_out.z = (speed > MAX_TH_VEL) ? MAX_TH_VEL : speed;
+            break;
+        }
+
+    case INACTIVE:
+    default:
+        msg_out.x = 0.f;
+        msg_out.y = 0.f;
+        msg_out.z = 0.f;
+        break;
     }
-    else if (abs(alpha) < ANGLE_ERROR_THRESHOLD) alpha = 0.f;
-    if (abs(eta) < ANGLE_ERROR_THRESHOLD) eta = 0.f;
 
-    float xvel = K1*rho;
-    float zvel = K2*alpha + K3*eta;
-
-    // ROS_INFO("(xvel, zvel) = %f %f", xvel, zvel);
-    if (xvel > MAX_X_VEL) msg_out.x = MAX_X_VEL;
-    else if (xvel < -MAX_X_VEL) msg_out.x = -MAX_X_VEL;
-    else msg_out.x = xvel;
-    if (zvel > MAX_TH_VEL) msg_out.z = MAX_TH_VEL;
-    else if (zvel < -MAX_TH_VEL) msg_out.z = -MAX_TH_VEL;
-    else msg_out.z = zvel;
-
-    // msg_out.x = (xvel > 3.0) ? 3.0 : xvel;
-    // msg_out.z = (zvel > 3.0) ? 3.0 : zvel;
-    ROS_INFO("msg_out = %f %f %f", msg_out.x, msg_out.y, msg_out.z);
+    // ROS_INFO("desired global x - curr pose x: %f - %f = %f", desired_global_x, curr_pose.x, dx);
+    // ROS_INFO("rho = %f", rho);
 
     local_vel_pub.publish(msg_out);
 
