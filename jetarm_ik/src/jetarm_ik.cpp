db45df2e44ef48445e4e2d413a82b3bb7ed7a071
Danny <dangfender935@gmail.com>
2026-03-29 21:38:43 +0000

    ADDED: Project 9 - Created jetarm_ik package

---
+++ jetarm_ik/src/jetarm_ik.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <Eigen/Dense>
+#include <geometry_msgs/Vector3.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "jetarm_ik");
+
+    ros::NodeHandle nodeHandle;
+}
