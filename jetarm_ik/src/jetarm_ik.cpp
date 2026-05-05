df46bf8daed463d908560a6b98776ac954a4f599
Danny <dangfender935@gmail.com>
2026-05-05 19:17:45 +0000

    UNFINISHED: Project 10 - Committed changes in anticipation of battery death - Wrote code to match the MATLAB script, which builds and runs - Code does not return the same values as the MATLAB script

--- jetarm_ik/src/jetarm_ik.cpp
+++ jetarm_ik/src/jetarm_ik.cpp
@@ -7,6 +7,7 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 
 #define JOINT1          (0)
 #define JOINT2          (1)
@@ -107,6 +108,9 @@ config_t calc_jetarm_ik(lambda_t lambda_d)
     int max_iter = 10000;
     int max_iter_q5 = 500;
     float error_threshold = 0.001;
+    float desired_yye = -1.0;
+    float yye = 0;
+    float yye_error = 0;
     q_t joint_limits[5][2] = {
         {JOINT1_MIN, JOINT1_MAX},
         {JOINT2_MIN, JOINT2_MAX},
@@ -115,130 +119,117 @@ config_t calc_jetarm_ik(lambda_t lambda_d)
         {JOINT5_MIN, JOINT5_MAX},
     };
 
+    Matrix<float, 4, 1> constrained_q;
+    
     lambda_t lambda_error, curr_lambda;
+    Matrix<float, 4, 1> constrained_error;
     config_t q;
-    jac_t J;
-    jac_t Jinv;
+    jac_t J, Jinv;
+    Matrix<float, 4, 4> constrained_J, constrained_Jinv;
+    std::vector<int> fk_indx{1, 2, 3, 4};
+    // ROS_INFO("Assigning constrained_q...");
     
     q << curr_config;
+    constrained_q << q(0),
+                     q(1),
+                     q(2),
+                     q(3);
     
     for (int i = 0; i < max_iter; i++)
     {
         curr_lambda << calc_endaffector_position(q);
         lambda_error << lambda_d - curr_lambda;
-        if (lambda_error.norm() <= error_threshold)
+        constrained_error << lambda_error(1),
+                            lambda_error(2),
+                            lambda_error(3),
+                            lambda_error(4);
+        if (constrained_error.norm() <= error_threshold)
             {break;}
-
-        // J << jacobian(q);
+        J << jacobian(q);
+        // ROS_INFO("assigning constrained_J...");
+        constrained_J << J(0, 0), J(0, 1), J(0, 2), J(0, 3),
+                         J(1, 0), J(1, 1), J(1, 2), J(1, 3),
+                         J(2, 0), J(2, 1), J(2, 2), J(2, 3),
+                         J(3, 0), J(3, 1), J(3, 2), J(3, 3);
 
         // Jinv << J.transpose() * (J*J.transpose()).inverse();
-
-        if (!Jinv.allFinite())
+        constrained_Jinv << constrained_J.inverse();
+        if (!constrained_Jinv.allFinite())
         {
-            // ROS_INFO("Reached Singularity!");
-            q << q + MatrixXf::Random(5, 1)*0.005;
+            ROS_INFO("Reached Singularity!");
+            constrained_q << constrained_q + MatrixXf::Random(4, 1)*0.005;
             continue;
         }
-        // q << q + K*Jinv*lambda_error;
+        // ROS_INFO("Assigning constrained_q in loop...");
+
+        constrained_q << constrained_q + K*constrained_Jinv*constrained_error;
+        // ROS_INFO("Adjusting constrained_q to be within joint limits...");
+
         for (int i = 0; i < 4; i++)
         {
             // Make sure each joint is within joint limits
-            q[i] = (q[i] > joint_limits[i][MAX]) ? 
+            constrained_q[i] = (constrained_q[i] > joint_limits[i][MAX]) ? 
                         joint_limits[i][MAX] : 
-                        (q[i] < joint_limits[i][MIN]) ?
+                        (constrained_q[i] < joint_limits[i][MIN]) ?
                             joint_limits[i][MIN] :
-                            q[i];
+                            constrained_q[i];
         } 
+        ROS_INFO("Assigning back to q...");
+
+        q << constrained_q(0),
+             constrained_q(1),
+             constrained_q(2),
+             constrained_q(3),
+             q(4);
+    }
+
+    for (int j = 0; j < max_iter_q5; j++)
+    {
+        curr_lambda << calc_endaffector_position(q);
+        yye = curr_lambda(4);
+        yye_error = desired_yye - yye;
+        if (abs(yye_error) < error_threshold)
+        {
+            break;
+        }
+        q(4) = q(4) - K2*yye_error;
     }
 
     return q;
 }
 
-void virtual_ik()
-{
-    config_t q;
-    jac_t J;
-    jac_t Jinv2;
-    lambda_t lambda_d, lambda_n, lambda_err;
-
-    q_t joint_limits[5][2] = {
-        {JOINT1_MIN, JOINT1_MAX},
-        {JOINT2_MIN, JOINT2_MAX},
-        {JOINT3_MIN, JOINT3_MAX},
-        {JOINT4_MIN, JOINT4_MAX},
-        {JOINT5_MIN, JOINT5_MAX},
-    };
-
-    q << -2.00, -1.65, 2.30, -1.9, 0.0;
-    ROS_INFO("initial [q0] = [%f %f %f %f %f]", q[0], q[1], q[2], q[3], q[4]);
-
-    // Make sure each joint is within joint limits
-    for (int i = 0; i < 4; i++)
-    {
-        // Make sure each joint is within joint limits
-        q[i] = (q[i] > joint_limits[i][MAX]) ? 
-                    joint_limits[i][MAX] : 
-                    (q[i] < joint_limits[i][MIN]) ?
-                        joint_limits[i][MIN] :
-                        q[i];
-    }
-
-    ROS_INFO("adjusted [q0] = [%f %f %f %f %f]", q[0], q[1], q[2], q[3], q[4]);
-
-    
-    lambda_n << calc_endaffector_position(curr_config);
-    ROS_INFO("initial endaffector position: [%f %f %f]", lambda_n(0), lambda_n(1), lambda_n(2));
-    
-    J << jacobian(q);    
-    ROS_INFO("Jacobian complete!");
-    
-    Jinv2 << J.inverse();
-    ROS_INFO("[Jinv] = [%f %f %f]", Jinv2(0, 0), Jinv2(0, 1), Jinv2(0, 2));
-    ROS_INFO("         [%f %f %f]", Jinv2(1, 0), Jinv2(1, 1), Jinv2(1, 2));
-    ROS_INFO("         [%f %f %f]", Jinv2(2, 0), Jinv2(2, 1), Jinv2(2, 2));
-    ROS_INFO("         [%f %f %f]", Jinv2(3, 0), Jinv2(3, 1), Jinv2(3, 2));
-    ROS_INFO("         [%f %f %f]", Jinv2(4, 0), Jinv2(4, 1), Jinv2(4, 2));
-    
-    lambda_d << 0.3737, 
-    0.0, 
-    0.10432, 
-    1.0, 
-    -1.0;
-    q << calc_jetarm_ik(lambda_d);
-    ROS_INFO("final [q] = [%f %f %f %f %f]", q[0], q[1], q[2], q[3], q[4]);
-}
-
 void set_joints(config_t q)
 {
-    ros::Rate r(0.5);
-    joint_pos_out.name = "joint1";
-    joint_pos_out.rad = q(0);
-    joint_pos_out.duration = 2.0;
-    set_joint_pub.publish(joint_pos_out);
+    ROS_INFO("final [q] = [%f %f %f %f %f]", q[0], q[1], q[2], q[3], q[4]);
+    // ros::Rate r(0.5);
+    // joint_pos_out.name = "joint1";
+    // joint_pos_out.rad = q(0);
+    // joint_pos_out.duration = 2.0;
+    // set_joint_pub.publish(joint_pos_out);
+    // // r.sleep();
+    
+    // joint_pos_out.name = "joint2";
+    // joint_pos_out.rad = q(1);
+    // joint_pos_out.duration = 2.0;
+    // set_joint_pub.publish(joint_pos_out);
+    // // r.sleep();
+    
+    // joint_pos_out.name = "joint3";
+    // joint_pos_out.rad = q(2);
+    // joint_pos_out.duration = 2.0;
+    // set_joint_pub.publish(joint_pos_out);
     // r.sleep();
     
-    joint_pos_out.name = "joint2";
-    joint_pos_out.rad = q(1);
-    joint_pos_out.duration = 2.0;
-    set_joint_pub.publish(joint_pos_out);
-    // r.sleep();
+    // joint_pos_out.name = "joint4";
+    // joint_pos_out.rad = q(3);
+    // joint_pos_out.duration = 2.0;
+    // set_joint_pub.publish(joint_pos_out);
+    // // r.sleep();
     
-    joint_pos_out.name = "joint3";
-    joint_pos_out.rad = q(2);
-    joint_pos_out.duration = 2.0;
-    set_joint_pub.publish(joint_pos_out);
-    r.sleep();
-    
-    joint_pos_out.name = "joint4";
-    joint_pos_out.rad = q(3);
-    joint_pos_out.duration = 2.0;
-    set_joint_pub.publish(joint_pos_out);
-    // r.sleep();
-    
-    joint_pos_out.name = "joint5";
-    joint_pos_out.rad = q(4);
-    joint_pos_out.duration = 2.0;
-    set_joint_pub.publish(joint_pos_out);
+    // joint_pos_out.name = "joint5";
+    // joint_pos_out.rad = q(4);
+    // joint_pos_out.duration = 2.0;
+    // set_joint_pub.publish(joint_pos_out);
 }
 
 void curr_config_recv_callback(const sensor_msgs::JointState& q_msg)
@@ -258,7 +249,7 @@ void curr_config_recv_callback(const sensor_msgs::JointState& q_msg)
 void desired_taskspace_recv_callback(const geometry_msgs::Vector3& lambda_d_msg)
 {
     lambda_t lambda_d;
-    // ROS_INFO("New desired taskspace received!");
+    ROS_INFO("New desired taskspace received!");
     lambda_d << lambda_d_msg.x, lambda_d_msg.y, lambda_d_msg.z, 1.0, -1.0;
     curr_config << calc_jetarm_ik(lambda_d);
     ROS_INFO("final [q] = [%f %f %f %f %f]", curr_config[0], curr_config[1], curr_config[2], curr_config[3], curr_config[4]);
@@ -290,7 +281,30 @@ void jac_recv_callback(const std_msgs::Float32MultiArray& msg)
     ROS_INFO("      [%f %f %f %f %f]", J(2, 0), J(2, 1), J(2, 2), J(2, 3), J(2, 4));
     ROS_INFO("      [%f %f %f %f %f]", J(3, 0), J(3, 1), J(3, 2), J(3, 3), J(3, 4));
     ROS_INFO("      [%f %f %f %f %f]", J(4, 0), J(4, 1), J(4, 2), J(4, 3), J(4, 4));
-    
+    ROS_INFO("");
+
+    curr_config << q;
+
+    Matrix<float, 4, 4> constrained_J;
+
+    constrained_J << J(0, 0), J(0, 1), J(0, 2), J(0, 3),
+                         J(1, 0), J(1, 1), J(1, 2), J(1, 3),
+                         J(2, 0), J(2, 1), J(2, 2), J(2, 3),
+                         J(3, 0), J(3, 1), J(3, 2), J(3, 3);
+
+    ROS_INFO("[constrained_J] = [%f %f %f %f]", constrained_J(0, 0), constrained_J(0, 1), constrained_J(0, 2), constrained_J(0, 3));
+    ROS_INFO("      [%f %f %f %f]", constrained_J(1, 0), constrained_J(1, 1), constrained_J(1, 2), constrained_J(1, 3));
+    ROS_INFO("      [%f %f %f %f]", constrained_J(2, 0), constrained_J(2, 1), constrained_J(2, 2), constrained_J(2, 3));
+    ROS_INFO("      [%f %f %f %f]", constrained_J(3, 0), constrained_J(3, 1), constrained_J(3, 2), constrained_J(3, 3));
+    ROS_INFO("");
+
+    Matrix<float, 4, 4> inv_J;
+    inv_J << constrained_J.inverse();
+    ROS_INFO("[inv_J] = [%f %f %f %f]", inv_J(0, 0), inv_J(0, 1), inv_J(0, 2), inv_J(0, 3));
+    ROS_INFO("          [%f %f %f %f]", inv_J(1, 0), inv_J(1, 1), inv_J(1, 2), inv_J(1, 3));
+    ROS_INFO("          [%f %f %f %f]", inv_J(2, 0), inv_J(2, 1), inv_J(2, 2), inv_J(2, 3));
+    ROS_INFO("          [%f %f %f %f]", inv_J(3, 0), inv_J(3, 1), inv_J(3, 2), inv_J(3, 3));
+    ROS_INFO("");
 }
 
 int main(int argc, char **argv)
@@ -304,11 +318,12 @@ int main(int argc, char **argv)
     // curr_config_sub = nodeHandle.subscribe("/joint_states", 1, curr_config_recv_callback);
     // ROS_INFO("Waiting for config space reading...");
     // while (!config_recv) ros::spinOnce();
-
     ROS_INFO("Ready!"); 
     desired_taskspace_sub = nodeHandle.subscribe("arm_desired_task_space", 1, desired_taskspace_recv_callback);
 
     jac_sub = nodeHandle.subscribe("jac_test", 1, jac_recv_callback);
+
+    
 
     while (ros::ok())
     {
